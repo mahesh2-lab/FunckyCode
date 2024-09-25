3d5f268ee8cddbac8e4a5e1183bfcebbfbd0af42
mahesh2-lab <maheshchopade133@gmail.com>
2024-09-25 14:27:16 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Home from './pages/Home'
+import React, { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import Warning from "./Warning";
 
 const App = () => {
-  return (
-    <>
-      <Home/>
-    </>
-  )
-}
+  const [isMobile, setIsMobile] = useState(false);
 
-export default App
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const mobileCheck = /Mobi|Android/i.test(userAgent);
+    setIsMobile(mobileCheck);
+  }, []);
+  return <>{isMobile ? <Warning /> : <Home />}</>;
+};
+
+export default App;
---
+++ src/Warning.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Warning = () => {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col w-1/3 p-5 rounded-lg shadow bg-white">
+          <div className="flex flex-col items-center text-center">
+            <div className="inline-block p-4 bg-yellow-50 rounded-full">
+              <svg
+                className="w-12 h-12 fill-current text-yellow-500"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M12 5.99L19.53 19H4.47L12 5.99M12 2L1 21h22L12 2zm1 14h-2v2h2v-2zm0-6h-2v4h2v-4z" />
+              </svg>
+            </div>
+            <h2 className="mt-2 font-semibold text-gray-800">
+              🚧 Website Under Development
+            </h2>
+            <p className="mt-2 text-sm text-gray-600 leading-relaxed">
+              We're working hard to bring you the best mobile experience. Stay
+              tuned as we fine-tune our website for your convenience.please
+              visit us on a laptop or desktop.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Warning;
